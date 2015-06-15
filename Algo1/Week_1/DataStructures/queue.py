42fb9edfb5ce34eb95566b68aa7afbc6e7dc16bf
presian <presiandanailov@gmail.com>
2015-06-15 18:05:32 +0300

    Remove some data

--- Algo1/Week_1/DataStructures/queue.py
+++ Algo1/Week_1/DataStructures/queue.py
@@ -3,8 +3,5 @@ from vector import Vector
 
 class Queue:
 
-	def __init__(self):
-		self.__queue = Vector()
-
-	def push(self, value):
-		self.__queue.add(value)
+    def __init__(self):
+        self.__queue = Vector()
