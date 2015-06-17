dd30b1bf1f124fb24d64a79ba0bb63ea814cb9cb
presian <presiandanailov@gmail.com>
2015-06-17 10:28:04 +0300

    ShellSort is done

--- Algo1/Week_1/Sorting/shell_sort.py
+++ Algo1/Week_1/Sorting/shell_sort.py
@@ -13,18 +13,23 @@ def shell_sort(arr):
         if first > second:
             arr[index] = second
             arr[index + gap] = first
-            pivot = index
-            while pivot > 1 and arr[pivot] < arr[pivot - 1]:
-                temp_var = arr[pivot]
-                arr[pivot] = arr[pivot - 1]
-                arr[pivot - 1] = temp_var
-                pivot -= 1
+            first_pivot = index
+            second_pivot = index + gap
+            while first_pivot >= 1 and arr[first_pivot] < arr[first_pivot - 1]:
+                temp_var = arr[first_pivot]
+                arr[first_pivot] = arr[first_pivot - 1]
+                arr[first_pivot - 1] = temp_var
+                first_pivot -= 1
+
+            while second_pivot >= 1 and arr[second_pivot] < arr[second_pivot - 1]:
+                temp_var = arr[second_pivot]
+                arr[second_pivot] = arr[second_pivot - 1]
+                arr[second_pivot - 1] = temp_var
+                second_pivot -= 1
 
         if index + gap * 2 < arr_len:
             index = index + gap
         else:
-            if index == 0:
-                return arr
             index = 0
             power += 1
             gap = make_gap(arr_len, power)
@@ -32,14 +37,14 @@ def shell_sort(arr):
 
 
 def make_gap(arr_len, power):
-    return arr_len // 2**power
+    return arr_len // 2 ** power
 
 
 def main():
-    # test_arr = [randint(0, 10) for x in range(0, 100)]
-    test_arr = [60, 84, 20, 35, 75, 73, 55, ]
+    test_arr = [randint(0, 100) for x in range(0, 1000)]
     print(test_arr)
     print(shell_sort(test_arr))
+
 
 if __name__ == '__main__':
     main()
