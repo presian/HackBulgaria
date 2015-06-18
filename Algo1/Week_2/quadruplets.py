89d5a95f1be54c05f8b492a8ecf1a880eac33ba7
presian <presiandanailov@gmail.com>
2015-06-18 14:09:36 +0300

    Add one more problem

--- Algo1/Week_2/quadruplets.py
+++ Algo1/Week_2/quadruplets.py
@@ -1,11 +1,16 @@
-from binary_search import binary_search
-
-
 def quadruplets(arr_one, arr_two, arr_three, arr_four):
-    arr_two = sorted(arr_two)
-    arr_three = sorted(arr_three)
-    arr_four = sorted(arr_four)
-    for e in arr_one:
-        q_sum = e
-        for i in range(0, len(arr_two)):
-            pass
+    counter = 0
+    for e_one in arr_one:
+        for e_two in arr_two:
+            for e_three in arr_three:
+                for e_four in arr_four:
+                    if e_one + e_two + e_three + e_four == 0:
+                        counter += 1
+    return counter
+
+
+def main():
+    print(quadruplets([5, 3, 4], [-2, -1, 6], [-1, -2, 4], [-1, -2, 7]))
+
+if __name__ == '__main__':
+    main()
