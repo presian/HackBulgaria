80120d44c0973a7409fcdc81a39b1bfe126e462c
presian <presiandanailov@gmail.com>
2015-06-22 11:43:20 +0300

    Klists is done

--- Algo1/Week_2/Thursday/k_lists.py
+++ Algo1/Week_2/Thursday/k_lists.py
@@ -1,14 +1,38 @@
-from heap import Heap
-
 class KLists:
 
-    def __init__(self, *lists):
-        self.__heap = Heap()
+    def __init__(self):
+        self.__lists = []
 
-    # Merge K sorted lists.
-    # lists - [Node]
-    def merge(lists):
-        pass
+    def merge(self, *lists):
+        lists = list(lists)
+        self.__lists = lists
+        final_arr_len = sum([len(l) for l in lists])
+        sorted_arr = []
+        while len(sorted_arr) < final_arr_len:
+            current_min = self.__get_current_min()
+            sorted_arr.append(current_min)
+        return sorted_arr
+
+    def __get_current_min(self):
+        lists_len = len(self.__lists)
+        first_members = []
+
+        for l in self.__lists:
+            first_members.append(l[0])
+
+        min_index = first_members.index(min(first_members))
+        min_value=first_members[min_index]
+        self.__lists[min_index].pop(0)
+
+        if len(self.__lists[min_index]) < 1:
+            self.__lists.pop(min_index)
+        return min_value
 
 
-# Ideqta e da v heap da se pazqt `stojnostite ot wseki ot listowete. Iprez nego da se wzima naj malkiq
+def main():
+    klists = KLists()
+    print(klists.merge([3, 5, 7, 9], [2, 4, 6], [0, 1, 8, 10], [-1, 15, 33, 21]))
+
+
+if __name__ == '__main__':
+    main()
