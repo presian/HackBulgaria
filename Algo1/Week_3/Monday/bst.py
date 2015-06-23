a5b2feb1c467c8296be8a234883ce437c2b685cf
presian <presiandanailov@gmail.com>
2015-06-24 00:16:44 +0300

    Add half working travers

--- Algo1/Week_3/Monday/bst.py
+++ Algo1/Week_3/Monday/bst.py
@@ -17,14 +17,14 @@ class BST:
             new_node = Node(value)
             while True:
                 if current_node.get_key() <= new_node.get_key():
-                    if self.__check_child_for_add(new_node, current_node, 'r'):
+                    if self.__check_child_for_add(current_node, 'r'):
                         current_node.add_rigth_child(new_node)
                         new_node.add_parent(current_node)
                         break
                     else:
                         current_node = current_node.get_right_child()
                 else:
-                    if self.__check_child_for_add(new_node, current_node, 'l'):
+                    if self.__check_child_for_add(current_node, 'l'):
                         current_node.add_left_child(new_node)
                         new_node.add_parent(current_node)
                         break
@@ -65,27 +65,44 @@ class BST:
                 parent_node.add_left_child(node.get_left_child())
             elif parent_node.get_right_child().get_key() == node.get_key():
                 parent_node.add_rigth_child(node.get_left_child())
-        # elif node.get_left_child() is None:
-        #     if parent_node.get_left_child().get_key() == node.get_key():
-        #         parent_node.add_left_child(node.get_right_child())
-        #     elif parent_node.get_right_child().get_key() == node.get_key():
-        #         parent_node.add_rigth_child(node.get_right_child())
         else:
             new_nod_key = self.__pop_most_left_from_node(node)
             node.add_key(new_nod_key)
-            # right_node = node.get_right_child()
-            # right_left_child = right_node.get_left_child()
-            # while right_left_child is not None:
-            #     right_node = right_left_child
-            #     right_left_child = right_node.get_left_child()
-            # node.add_key(right_node.get_key())
-            # up_child = right_node.get_right_child()
-            # parent_right_left_child = right_node.get_parent()
-            # if up_child is not None:
-            #     parent_right_left_child.add_left_child(up_child)
-            #     up_child.add_parent(parent_right_left_child)
-            # else:
-            #     parent_right_left_child.add_left_child(None)
+
+    def traverse(self):
+        self.__traverse(self.__root)
+
+    def __traverse(self, node):
+        # if node is None:
+        #     node = self.__root
+        visited_rights = []
+        left_child = self.__get_most_left_node_from_node(node)
+
+        # TODO: check for None (left_child)
+        # TODO: add check for right_child on last right element
+        if left_child is not None:
+            print(left_child.get_key())
+            right_child = left_child.get_right_child()
+            if right_child is not None:
+                self.__traverse(right_child)
+            parent = left_child.get_parent()
+            print(parent.get_key())
+            parent_right = parent.get_right_child()
+            self.__traverse(parent_right)
+        else:
+            if node is not None:
+                print(node.get_key())
+
+    def __get_most_left_node_from_node(self, node):
+        if node is not None:
+            child = node.get_left_child()
+            if child is not None:
+                next_left = child.get_left_child()
+                while next_left is not None:
+                    child = next_left
+                    next_left = next_left.get_left_child()
+            return child
+        return None
 
     def __pop_most_left_from_node(self, node):
         child = node.get_right_child()
@@ -96,9 +113,8 @@ class BST:
         result_key = child.get_key()
         up_child = child.get_right_child()
         child_parent = child.get_parent()
-        print(child_parent)
         if up_child is not None:
-            child_parent.add_left_child(up_child)
+            child_parent.add_rigth_child(up_child)
             up_child.add_parent(child_parent)
         else:
             if child_parent.get_parent() is None:
@@ -107,7 +123,7 @@ class BST:
                 child_parent.add_left_child(None)
         return result_key
 
-    def __check_child_for_add(self, new_node, current_node, child_type):
+    def __check_child_for_add(self, current_node, child_type):
         if child_type == 'r':
             if current_node.get_right_child() is None:
                 return True
@@ -120,26 +136,35 @@ class BST:
     def get_bst(self):
         return self.__root
 
+# TODO: Some corner case in right half
+
 
 def main():
     bst = BST()
 
-    bst.isert(9)
-    bst.isert(6)
-    bst.isert(8)
-    bst.isert(7)
-    bst.isert(6)
-    bst.isert(5)
-    bst.isert(11)
-    bst.isert(10)
-    bst.isert(18)
-    bst.isert(15)
+    # bst.isert(9)
+    # bst.isert(6)
+    # bst.isert(8)
+    # bst.isert(7)
+    # bst.isert(6)
+    # bst.isert(5)
+    # bst.isert(11)
+    # bst.isert(10)
+    # bst.isert(18)
+    # bst.isert(15)
 
-    bst.remove(9)
-    print(bst.get_bst())
-    bst.remove(10)
-
-    print(bst.get_bst())
+    # bst.isert(7)
+    # bst.isert(1)
+    # bst.isert(6)
+    # bst.isert(2)
+    # bst.isert(4)
+    # bst.isert(3)
+    # bst.isert(5)
+    # bst.isert(11)
+    # bst.isert(15)
+    # bst.isert(18)
+    bst.traverse()
+    # print(bst.get_bst())
 
 if __name__ == '__main__':
     main()
