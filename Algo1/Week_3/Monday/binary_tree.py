61fafbe7bb9377e976fcb38907f97397edbcaf0a
presian <presiandanailov@gmail.com>
2015-06-25 17:08:36 +0300

    Add some problems

---
+++ Algo1/Week_3/Monday/binary_tree.py
@@ -0,0 +1,11 @@
+from node import Node
+
+class BinaryThree:
+
+    def __init__(self):
+        self.__root = None
+
+    def insert(self, value):
+        if self.__root is None:
+            self.__root = Node(value)
+        else:
